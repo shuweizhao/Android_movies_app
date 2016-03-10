8480b5030cc18b7563e317a78ffbad8f912bf86c
szhao8 <szhao8@ncsu.edu>
2016-03-10 13:33:46 -0500

    finished detail UI

--- app/src/main/java/com/example/shuweizhao/movies/DetailFragment.java
+++ app/src/main/java/com/example/shuweizhao/movies/DetailFragment.java
@@ -1,6 +1,7 @@
 package com.example.shuweizhao.movies;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
 
 /**
  * Created by shuweizhao on 3/7/16.
@@ -19,11 +22,22 @@ public class DetailFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.detail_fragment, container, false);
         Intent intent = getActivity().getIntent();
+        String[] params = null;
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
             String movieInfo = intent.getStringExtra(Intent.EXTRA_TEXT);
-            TextView tv = (TextView) rootView.findViewById(R.id.movieDetailInfo);
-            tv.setText(movieInfo);
+            params = movieInfo.split("\n");
         }
+        TextView overview = (TextView) rootView.findViewById(R.id.overview_text);
+        SimpleDraweeView poster = (SimpleDraweeView) rootView.findViewById(R.id.detail_poster);
+        TextView title = (TextView)rootView.findViewById(R.id.detail_title);
+        TextView vote_count = (TextView) rootView.findViewById(R.id.vote_average);
+        TextView release_date = (TextView) rootView.findViewById(R.id.release_date);
+        title.setText("Title\n" + params[0]);
+        Uri uri = Uri.parse("http://image.tmdb.org/t/p/w342" + params[1]);
+        poster.setImageURI(uri);
+        overview.setText(params[2]);
+        vote_count.setText("Vote Average\n" + params[3]);
+        release_date.setText("Release Date\n" + params[4]);
         return rootView;
     }
 }
